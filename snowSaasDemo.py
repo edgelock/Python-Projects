5e3dcd2796958b4bf0aeadea350ea4840b7b9468
edgelock <44417754+edgelock@users.noreply.github.com>
2021-01-28 22:13:44 -0500

    Add files via upload

---
+++ snowSaasDemo.py
@@ -0,0 +1,26 @@
+#Need to install requests package for python
+#easy_install requests
+#this script creates an incident within snow
+import requests
+
+# Set the request parameters
+url = 'https://dev85085.service-now.com/api/now/table/incident?sysparm_display_value=true&sysparm_exclude_reference_link=true&sysparm_fields=short_description%2Cnumber'
+
+# Eg. User name="admin", Password="admin" for this code sample.
+user = 'admin'
+pwd = 'admin'
+
+# Set proper headers
+headers = {"Content-Type":"application/json","Accept":"application/json"}
+
+# Do the HTTP request
+response = requests.post(url, auth=(user, pwd), headers=headers ,data="{\"short_description\":\"test via python\"}")
+
+# Check for HTTP codes other than 200
+if response.status_code != 200: 
+    print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:',response.json())
+    exit()
+
+# Decode the JSON response into a dictionary and use the data
+data = response.json()
+print(data)
