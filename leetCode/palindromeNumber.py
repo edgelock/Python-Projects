727cd4d800495950f300bb584e4f7a32bf310e5c
edgelock <44417754+edgelock@users.noreply.github.com>
2021-12-08 17:19:30 -0500

    Updated with comments

--- leetCode/palindromeNumber.py
+++ leetCode/palindromeNumber.py
@@ -4,18 +4,25 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
+        #Basic check to see if the number is less than 0
         if x < 0: return False
-        
+        #Set the divider value.
+        #It could be 10,100...
+
         div = 1
+        #If our x is greater than 10 time the div we'll increase the div
         while x >= 10 * div:
             div *= 10 
         
         while x:
+            #How to get the right digit
             right = x % 10
+            #How to get the left digit
             left = x // div
-            
+            #Check if left does NOT equal right
             if left != right: return False
-            
+            #Chop off the left and right digit
             x = (x % div) // 10
+            #Chop off 2 digits from the div
             div = div / 100 
         return True
